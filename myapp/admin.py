3e80a5236ef3261afdf8acd0c77fcc15440a4ace
sahirul70 <sahirulislam.70@gamil.com>
2021-12-09 21:14:29 +0600

    first update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.db import models
+from .models import Author,Catagory,Journal
 
 # Register your models here.
+
+
+
+admin.site.register(Author)
+admin.site.register(Catagory)
+admin.site.register(Journal)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+class Catagory(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+class Journal(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    catagory =models.ManyToManyField(Catagory)
+    publish_date =models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    reviewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
